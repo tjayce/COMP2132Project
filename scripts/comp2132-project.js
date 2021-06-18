e5bec25c80be42eb7536ec4e9a17b4d7149a98c0
tjayce <theo.chiu@hotmail.com>
2021-06-17 17:30:25 -0700

    Start

---
+++ scripts/comp2132-project.js
@@ -0,0 +1,18 @@
+/*
+const newGame    = document.getElementById("new-game");
+const rollDice     = document.getElementById("roll-dice");
+*/ 
+
+let toggleStatus = false;
+
+/*
+const $btnToggle = $('#hamburger');
+
+$btnToggle.click(function(){
+    $(".side-bar").show;
+});
+*/
+
+const sideBar = document.getElementById("side-bar");
+
+sideBar.style.visibility = "visible";
