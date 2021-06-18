b92e7dd53746ef75e8d65092923d48d02347e506
tjayce <theo.chiu@hotmail.com>
2021-06-17 20:39:19 -0700

    Phase 1

--- scripts/comp2132-project.js
+++ scripts/comp2132-project.js
@@ -1,18 +1,68 @@
-/*
-const newGame    = document.getElementById("new-game");
-const rollDice     = document.getElementById("roll-dice");
-*/ 
 
-let toggleStatus = false;
+const btnNewGame   = document.getElementById("new-game");
+const btnRoll    = document.getElementById("roll-dice");
 
-/*
-const $btnToggle = $('#hamburger');
 
-$btnToggle.click(function(){
-    $(".side-bar").show;
-});
-*/
+let toggleStatusOpen = false;
 
+const hamburger = document.getElementById("hamburger")
 const sideBar = document.getElementById("side-bar");
 
-sideBar.style.visibility = "visible";
+
+//console.log(toggleStatusOpen);
+
+hamburger.addEventListener('click', function(){
+    if(toggleStatusOpen === false) {
+        sideBar.style.visibility = "visible";
+        toggleStatusOpen = true;
+    } else {
+        sideBar.style.visibility = "hidden";
+        toggleStatusOpen = false;
+    }   
+});
+
+//console.log(toggleStatusOpen);
+
+/*--------------
+   Dice Game
+---------------*/
+/*
+class Dice {
+    constructor(value){
+        this.value = value;
+    }
+}
+
+const dice01 = new Dice();
+const dice02 = new Dice();
+*/
+
+const dice01 = document.getElementById("dice01");
+const dice02 = document.getElementById("dice02");
+const result = document.getElementById("result");
+const score = document.getElementById("score");
+let counter = 0;
+let total = 0;
+
+function rollDice() {
+    const d01 = Math.floor(Math.random()*6) +1;
+    const d02 = Math.floor(Math.random()*6) +1;
+    total = d01 + d02;
+    
+    dice01.innerHTML = d01;
+    dice02.innerHTML = d02;
+
+    result.innerHTML = total;
+    score.innerHTML = counter += total;
+}
+
+
+btnRoll.addEventListener('click', rollDice);
+
+btnNewGame.addEventListener('click', function(){
+    dice01.innerHTML = 0;
+    dice02.innerHTML = 0;
+    result.innerHTML = 0;
+    counter = 0;
+    score.innerHTML = 0;
+});
