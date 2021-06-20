50aa232d0e86cdfba082e2cd93d4818abb5ea7fd
tjayce <theo.chiu@hotmail.com>
2021-06-20 08:56:49 -0700

    Phase 2

--- scripts/comp2132-project.js
+++ scripts/comp2132-project.js
@@ -1,7 +1,7 @@
 
-const btnNewGame   = document.getElementById("new-game");
-const btnRoll    = document.getElementById("roll-dice");
-
+/*----------------------------------
+             Side Bar
+-----------------------------------*/
 
 let toggleStatusOpen = false;
 
@@ -12,32 +12,81 @@ const sideBar = document.getElementById("side-bar");
 //console.log(toggleStatusOpen);
 
 hamburger.addEventListener('click', function(){
+    
     if(toggleStatusOpen === false) {
         sideBar.style.visibility = "visible";
+        sideBar.style.left = "80%";
         toggleStatusOpen = true;
     } else {
-        sideBar.style.visibility = "hidden";
+        sideBar.style.left = "100%";
         toggleStatusOpen = false;
     }   
 });
 
 //console.log(toggleStatusOpen);
 
-/*--------------
-   Dice Game
----------------*/
-/*
+/*-----------------------------------
+             Dice Game
+------------------------------------*/
+const btnNewGame   = document.getElementById("new-game");
+const btnRoll    = document.getElementById("roll-dice");
+
+const out01 = document.getElementById("dice01");
+
+let dice;
+
 class Dice {
-    constructor(value){
-        this.value = value;
+    constructor(){}
+
+    roll(){
+        dice = Math.floor(Math.random()*6) +1;
+        return dice;
     }
 }
+const p1d1 = new Dice();
+const p1d2 = new Dice();
 
-const dice01 = new Dice();
-const dice02 = new Dice();
-*/
+const p2d1 = new Dice();
+const p2d2 = new Dice();
 
-const dice01 = document.getElementById("dice01");
+
+function rollDice(){
+    p1d1.roll();
+    p1d2.roll();
+
+    p2d1.roll();
+    p2d2.roll();
+}
+
+btnRoll.addEventListener('click', rollDice);
+
+console.log(p1d1.roll());
+console.log(p1d2.roll());
+console.log(p2d1.roll());
+console.log(p2d2.roll());
+
+
+out01.innerHTML = temp1 ;
+
+//------------
+function newGame(){
+    //set everything to 0/default
+    /*
+    dice01.innerHTML = 0;
+    dice02.innerHTML = 0;
+    result.innerHTML = 0;
+    counter = 0;
+    score.innerHTML = 0;
+    */
+}
+
+btnNewGame.addEventListener('click', function(){
+    newGame();
+});
+
+
+/* trash
+const out01 = document.getElementById("dice01");
 const dice02 = document.getElementById("dice02");
 const result = document.getElementById("result");
 const score = document.getElementById("score");
@@ -66,3 +115,4 @@ btnNewGame.addEventListener('click', function(){
     counter = 0;
     score.innerHTML = 0;
 });
+*/
